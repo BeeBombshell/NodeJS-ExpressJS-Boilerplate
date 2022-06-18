8ac0d4cb1bb77e95283e4cd03d178d86a424e207
Bhavya Verma <vbhavya269@gmail.com>
2022-06-18 19:25:38 +0530

    🎉 Added 2 missing files

---
+++ first module/1-intro.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if (amount < 10) {
+    console.log('Small Number')
+} else {
+    console.log('Big Number')
+}
+
+console.log(`Hey it's my first node application!!!`)
---
+++ first module/2-globals.js
@@ -0,0 +1,14 @@
+//GLOBALS
+
+//__dirname
+//__filename
+//require
+//module
+//process
+
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('Hello World')
+}, 1000
+)
